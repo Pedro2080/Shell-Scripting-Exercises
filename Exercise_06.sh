c775cfcc60aed297457e2cdc99b6461741e004d4
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-12-13 16:48:17 +0100

    Added Exercise_06

---
+++ Exercise_06.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+<<mycom
+  Write a shell script that displays, “This script will exit with 0 exit status.” Be sure that the script does indeed exit with a 0 exit status.
+mycom
+
+echo "This script will exit with 0 exit status."
+exit 0
