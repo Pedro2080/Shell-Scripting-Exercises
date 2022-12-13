859b4ceb1c9ef6efd6f933f50ee078545536eaf0
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-12-13 19:13:04 +0100

    Added Exercise_08

---
+++ Exercise_08.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+<<mycom
+  Write a script that executes the command “cat/etc/shadow”. If the command return a 0 exit status, report “command succeeded” and exit with a 0 exit status.
+If the command returns a non-zero exit status, report “Command failed” and exit with a 1 exit status.
+mycom
+
+cat /etc/shadow
+
+if [ "$?" -eq 0 ]
+then
+  echo "command succeeded"
+  exit 0
+else
+  echo "Command failed"
+  exit 1
+fi
