1e35e462cc184b4dac79edfee69b625c020bbf69
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-12-13 13:18:00 +0100

    Added Exercise_01

---
+++ Exercise_01.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+<<mycom
+Write a shell script to check if the file “file_path” exists. If it does exist and is not empty, display “file_path passwords are enabled,the file is not empty.”
+Next, check if you can write to the file. If you can, display “You have permissions to edit “file_path.””If you cannot, display
+“You do NOT have permissions to edit “file_path””.
+mycom
+
+file_path="/home/joao/Desktop/hakuna_matata.txt"
+
+if [ -e "$file_path" ] && [ -s "$file_path"]
+then 
+	echo "$file_path password are enabled, the file is not empty"
+fi
+
+if [ -x $file_path ]
+then
+	echo "You have permission to execute the $file_path"
+else
+	echo "You dont have permission to execute $file_path"
+fi	
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Shell-Scripting-Exercises
 
-**1-** Write a shell script to check if the file “file_path” exists. If it does exist, display “file_path passwords are enabled.”
+**1-** Write a shell script to check if the file “file_path” exists.If it does exist and is not empty, display “file_path passwords are enabled,thefile is not empty.”
 Next, check if you can write to the file. If you can, display “You have permissions to edit “file_path.””If you cannot, display
 “You do NOT have permissions to edit “file_path””.
 
