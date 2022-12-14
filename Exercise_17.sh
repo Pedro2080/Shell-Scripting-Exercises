1c5b6c5148f0d31ae1d9ca4c88715e131e6693f3
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-12-14 15:02:49 +0100

    Added Exercise_17

---
+++ Exercise_17.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash -x
+
+<<mycom
+Modify the previous exercise so that script continuous, even if an error occurs. This time, all three ls command will execute.
+mycom
+
+ls /hms
+ls /hms/ms
+ls /hms/apps
